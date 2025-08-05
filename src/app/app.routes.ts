a5c870b05dbbf4f92c5c52fed474d1b44c526522
FranCampanoDiaz <campanodiazfrancisco@gmail.com>
2025-08-05 16:32:36 +0200

    Creación de proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sobre-mi', component: AboutComponent },
+  { path: 'logros', component: AchievementsComponent },
+  { path: 'galeria', component: GalleryComponent },
+  { path: 'contacto', component: ContactFormComponent },
+];
