96a463ffba9b8f12353e755e94e828f78355b167
FranCampanoDiaz <campanodiazfrancisco@gmail.com>
2025-08-08 22:38:56 +0200

    Arreglado carrousel y botón menu

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
