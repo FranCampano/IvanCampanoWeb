d8bb2e8771457cfd7fb4576770ca0b23235bebff
FranCampanoDiaz <campanodiazfrancisco@gmail.com>
2025-08-10 19:37:39 +0200

    Footer realizado

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,18 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,NgFor],
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
-
+  menuItems = [
+    { label: 'Quién soy', link: '/sobre-mi' },
+    { label: 'Patrocinadores', link: '/partners' },
+    { label: 'Servicios', link: '/servicios' },
+    { label: 'Contacto', link: '/contacto' }
+  ];
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,20 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive, NgFor],
   templateUrl: './menu.component.html',
 })
 export class MenuComponent {
+  menuItems = [
+    { label: 'Inicio', link: '/' },
+    { label: 'Quién soy', link: '/sobre-mi' },
+    { label: 'Patrocinadores', link: '/partners' },
+    { label: 'Servicios', link: '/servicios' },
+    { label: 'Contacto', link: '/contacto' }
+  ];
 
 }
